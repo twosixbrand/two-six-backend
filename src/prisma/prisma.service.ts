6cc30f641971cf4dccabbd91547c9c51cd4337de
Jorge A Manrique <jamanrique@gmail.com>
2026-03-02 23:59:40 -0500

    sincronizar la BD

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,19 +4,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({
-      log: ['error', 'warn'],
-      datasources: {
-        db: {
-          url: process.env.DATABASE_URL
-            ? `${process.env.DATABASE_URL}${process.env.DATABASE_URL.includes('?') ? '&' : '?'}connection_limit=3&pool_timeout=5`
-            : undefined,
-        },
-      },
-    });
-  }
-
   async onModuleInit() {
     // Conecta a la base de datos cuando el módulo se inicializa
     await this.$connect();
